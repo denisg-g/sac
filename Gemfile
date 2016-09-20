0b672cdfcd499474af10388ff85ff81d295bd598
JarvisNic <aaronmerk2loke9624@gmail.com>
2016-09-20 11:38:45 -0600

    devise y heroku

--- Gemfile
+++ Gemfile
@@ -26,11 +26,18 @@ gem 'sdoc', '~> 0.4.0', group: :doc
 # Use ActiveModel has_secure_password
 # gem 'bcrypt', '~> 3.1.7'
 
+
+#use the gem for authenticathion users! 
+
+gem 'devise'
 # Use Unicorn as the app server
 # gem 'unicorn'
 
 # Use Capistrano for deployment
 # gem 'capistrano-rails', group: :development
+
+gem 'rails_12factor', group: :production  
+gem 'unicorn'
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  get 'welcome/index'
+
+  devise_for :users
+  
+ post 'signup' => 'devise/registrations#create', :as => :custom_user_registration
   resources :groups
   resources :registers
   resources :students
@@ -8,7 +13,7 @@ Rails.application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+   root 'welcome#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
